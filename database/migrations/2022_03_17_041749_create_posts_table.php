730cf735f5cae316fe08b469dccafefafea8a3b4
ShotaAyabe <a.s.1030.eren21s@gmail.com>
2022-03-20 20:25:08 +0900

    #1 Migrationの改修完了

--- database/migrations/2022_03_17_041749_create_posts_table.php
+++ database/migrations/2022_03_17_041749_create_posts_table.php
@@ -1,10 +1,10 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
-class CreateTweetsTable extends Migration
+class CreatePostsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateTweetsTable extends Migration
      */
     public function up()
     {
-        Schema::create('tweets', function (Blueprint $table) {
+        Schema::create('posts', function (Blueprint $table) {
             $table->increments('id');
             $table->unsignedInteger('user_id')->comment('ユーザID');
             $table->string('text')->comment('本文');
@@ -23,13 +23,13 @@ class CreateTweetsTable extends Migration
             $table->index('id');
             $table->index('user_id');
             $table->index('text');
-            
-    /**usersテーブルと外部キー接続を宣言*/
+
             $table->foreign('user_id')
                 ->references('id')
                 ->on('users')
                 ->onDelete('cascade')
                 ->onUpdate('cascade');
+            
         });
     }
 
@@ -40,6 +40,6 @@ class CreateTweetsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('tweets');
+        Schema::dropIfExists('posts');
     }
 }
